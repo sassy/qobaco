5a45dc545c2d58c6f6b6495b958b6e77c6c40fc3
Satoshi Watanabe <sassy.watanabe@gmail.com>
2023-03-03 22:41:38 +0900

    詳細ページを作成

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,17 @@
-<script>
+<script lang="ts">
+	import { goto } from '$app/navigation';
+
+	function navigateToDetail(id: number) {
+    	goto(`/questions/${id}`);
+  	}
+
+	let questions = [
+	{id: 1, title: "最初の質問"},
+	{id: 2, title: "2番目の質問"},
+	{id: 3, title: "3番目の質問"},
+	{id: 4, title: "4番目の質問"},
+	{id: 5, title: "5番目の質問"},
+	];
 </script>
 
 <svelte:head>
@@ -14,6 +27,14 @@
 	<h2>
 		みんなから質問を集める、質問箱サービス
 	</h2>
-
-	作成中
+	<ul class="flex flex-col">
+		{#each questions as question}
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
+			<li 
+				class="mx-5 my-2 py-5 border-2 rounded-sm"
+				on:click={() => navigateToDetail(question.id)}>
+				{question.title}
+			</li>
+		{/each}
+	</ul>
 </section>
