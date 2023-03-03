9620c76db72a0efba6276c13abeeee3498ebf7dc
Satoshi Watanabe <sassy.watanabe@gmail.com>
2023-03-03 22:17:51 +0900

    TailwindCSSを導入

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+    mode: 'jit',
+    purge: ['./src/**/*.svelte'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
